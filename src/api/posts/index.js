cd45ba3d3fe19954bb5d4f9c32136fc034963370
DJArgoho <djargoho@gmail.com>
2019-12-11 22:19:36 +0900

    mongo

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,21 +1,35 @@
+// const Router = require('koa-router');
+// const posts = new Router();
+// const { list, read, remove, replace, update, write } = require('./posts.ctrl');
+
+// //post 관련 function을 한 곳에서 관리 할 수 있도록 만듬 (이런식으로도 할 수 이똬라는 의미)
+// const printInfo = ctx => {
+//   ctx.body = {
+//     method: ctx.method,
+//     path: ctx.path,
+//     params: ctx.params,
+//   };
+// };
+
+// posts.get('/', list);
+// posts.get('/:id', read);
+// posts.post('/', write);
+// posts.delete('/:id', remove);
+// posts.put('/:id', replace);
+// posts.patch('/:id', update);
+
+// module.exports = posts;
+
+//mongoDB 사용
+
 const Router = require('koa-router');
 const posts = new Router();
-const { list, read, remove, replace, update, write } = require('./posts.ctrl');
-
-//post 관련 function을 한 곳에서 관리 할 수 있도록 만듬 (이런식으로도 할 수 이똬라는 의미)
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
+const { list, read, remove, update, write } = require('./posts.ctrl');
 
 posts.get('/', list);
 posts.get('/:id', read);
 posts.post('/', write);
 posts.delete('/:id', remove);
-posts.put('/:id', replace);
 posts.patch('/:id', update);
 
 module.exports = posts;
--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,147 +1,183 @@
-let postId = 1; //id 초기값
+// let postId = 1; //id 초기값
 
-//posts 배열 초기 데이터 (우선 db가 없으니까..)
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용',
-  },
-];
+// //posts 배열 초기 데이터 (우선 db가 없으니까..)
+// const posts = [
+//   {
+//     id: 1,
+//     title: '제목',
+//     body: '내용',
+//   },
+// ];
+
+// /*
+//     포스트 작성
+//     POST /posts
+// */
+
+// const write = ctx => {
+//   //Rest API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
+//   const { title, body } = ctx.request.body;
+
+//   postId += 1;
+
+//   const post = { id: postId, title, body };
+//   posts.push(post);
+//   ctx.body = post;
+// };
+
+// /*
+//     포스트 목록 조회
+//     GET /posts
+
+// */
+// const list = ctx => {
+//   ctx.body = posts;
+// };
+
+// /*
+//     특정 포스트 조회
+//     GET /posts/:id
+
+// */
+
+// const read = ctx => {
+//   const { id } = ctx.params;
+
+//   //주어진 id 값으로 포스트를 찾는다.
+//   //파라미터로 받아온 값은 문자열이므로 파라미터를 숫자로 변환하거나 비교할 p.id 값을 문자열로 바꿔야함.
+//   const post = posts.find(p => p.id.toString() === id);
+
+//   if (!post) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다.',
+//     };
+//     return;
+//   }
+
+//   ctx.body = post;
+// };
+
+// /*
+//     특정 포스터 제거
+//     DELETE posts/:id
+// */
+
+// const remove = ctx => {
+//   const { id } = ctx.params;
+
+//   const index = posts.findIndex(p => p.id.toString() === id);
+
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '포스트가 존재하지 않습니다. ',
+//     };
+
+//     return;
+//   }
+
+//   posts.splice(index, 1);
+//   ctx.status = 204; //No Content를 의미함
+// };
+
+// /*  포스트 수정(통째로 교체)
+//     PUT /posts/:id
+
+//     {title, body}
+// */
+// const replace = ctx => {
+//   const { id } = ctx.params;
+
+//   const index = posts.findIndex(p => p.id.toString() === id);
+
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '해당 포스트가 존재하지 않습니다.',
+//     };
+
+//     return;
+//   }
+
+//   posts[index] = {
+//     id,
+//     ...ctx.request.body,
+//   };
+
+//   ctx.body = posts[index];
+// };
+
+// /* 포스트 일부 수정(일부분만 교체)
+//     PATCH /posts/:id
+
+//     {title, body}
+
+// */
+// const update = ctx => {
+//   //PATCH 메서드는 주어진 필드만 교체한다.
+//   const { id } = ctx.params;
+
+//   const index = posts.findIndex(p => p.id.toString() === id);
+
+//   if (index === -1) {
+//     ctx.status = 404;
+//     ctx.body = {
+//       message: '해당 포스트가 존재하지 않습니다.',
+//     };
+
+//     return;
+//   }
+
+//   posts[index] = {
+//     ...posts[index],
+//     ...ctx.request.body,
+//   };
+
+//   ctx.body = posts[index];
+// };
+
+// module.exports = {
+//   write,
+//   list,
+//   read,
+//   remove,
+//   replace,
+//   update,
+// };
+
+//위의 내용을 mongodb와 연결해서 해보자
+const Post = require('models/post');
+//put은 구현 안함
 
 /*
-    포스트 작성 
-    POST /posts
+    write 함수 
+    POST
 */
+const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
 
-const write = ctx => {
-  //Rest API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-  const { title, body } = ctx.request.body;
+    
+  //새 POST 인스턴스를 만든다.
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
 
-  postId += 1;
 
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-};
+  try {
+    await post.save(); //데이터 베이스에 등록한다. //저장된 결과를 반환
 
-/*
-    포스트 목록 조회 
-    GET /posts
-
-*/
-const list = ctx => {
-  ctx.body = posts;
-};
-
-/*
-    특정 포스트 조회 
-    GET /posts/:id
-
-*/
-
-const read = ctx => {
-  const { id } = ctx.params;
-
-  //주어진 id 값으로 포스트를 찾는다.
-  //파라미터로 받아온 값은 문자열이므로 파라미터를 숫자로 변환하거나 비교할 p.id 값을 문자열로 바꿔야함.
-  const post = posts.find(p => p.id.toString() === id);
-
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+    // eslint-disable-next-line require-atomic-updates
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(error, 500);
   }
-
-  ctx.body = post;
 };
+const list = ctx => {};
+const read = ctx => {};
+const remove = ctx => {};
+const update = ctx => {};
 
-/*
-    특정 포스터 제거 
-    DELETE posts/:id
-*/
-
-const remove = ctx => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다. ',
-    };
-
-    return;
-  }
-
-  posts.splice(index, 1);
-  ctx.status = 204; //No Content를 의미함
-};
-
-/*  포스트 수정(통째로 교체) 
-    PUT /posts/:id
-
-    {title, body}
-*/
-const replace = ctx => {
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '해당 포스트가 존재하지 않습니다.',
-    };
-
-    return;
-  }
-
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-
-  ctx.body = posts[index];
-};
-
-/* 포스트 일부 수정(일부분만 교체) 
-    PATCH /posts/:id
-
-    {title, body}
-
-*/
-const update = ctx => {
-  //PATCH 메서드는 주어진 필드만 교체한다.
-  const { id } = ctx.params;
-
-  const index = posts.findIndex(p => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '해당 포스트가 존재하지 않습니다.',
-    };
-
-    return;
-  }
-
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-
-  ctx.body = posts[index];
-};
-
-module.exports = {
-  write,
-  list,
-  read,
-  remove,
-  replace,
-  update,
-};
+module.exports = { write, list, read, remove, update };
